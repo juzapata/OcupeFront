b7d388682c160f9dbfd99f8f942576136ff8b866
sirleyalmeida <sirleyas90@gmail.com>
2021-01-16 14:03:14 -0300

    create button initial

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.scss';
+import '../../styles/main.scss';
+import Button from '../../components/base/Button';
 
 const App = () => {
+
   return (
     <div className="container">
       <h1>Ocupe</h1>
+      <Button 
+        type="submit"
+        classNameBtn="btn__primary btn__primary--bigger"
+        text="Entrar"
+      />
     </div>
   );
 }
