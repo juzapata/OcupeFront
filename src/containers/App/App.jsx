8d60fcece383588123371266addbbfbc9479aea7
sirleyalmeida <sirleyas90@gmail.com>
2021-01-16 11:59:54 -0300

    add structure files

---
+++ src/containers/App/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Ocupe</h1>
+    </div>
+  );
+}
+
+export default App;
