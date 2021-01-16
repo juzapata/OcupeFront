6d079c17d87c1cc5d02e124fea6b8d1263fb825e
sirleyalmeida <sirleyas90@gmail.com>
2021-01-16 17:56:52 -0300

    create button, button + icon and cotainer track

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import './App.scss';
 import '../../styles/main.scss';
-import Button from '../../components/base/Button';
+import Routes from '../../routes';
 
 const App = () => {
 
   return (
     <div className="container">
-      <h1>Ocupe</h1>
-      <Button 
-        type="submit"
-        classNameBtn="btn__primary btn__primary--bigger"
-        text="Entrar"
-      />
+      <Routes/>
     </div>
   );
 }
---
+++ src/routes/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import { useHistory } from "react-router-dom";
+import Track from '../../containers/Track';
+
+const Home = () => {
+
+  return (    
+    <>
+      <Track/>
+    </>
+  )
+}
+
+export default Home;
