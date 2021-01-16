519fc4df5134a39f70e98e60c26420176de3b7df
Tatiane Mendes Camillo <tatiane@Alexs-MacBook-Pro.local>
2021-01-16 15:40:01 -0300

    utilizado variáveis

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.scss';
 import '../../styles/main.scss';
 import Button from '../../components/base/Button';
+import Input from '../../components/base/Input';
 
 const App = () => {
 
@@ -13,6 +14,11 @@ const App = () => {
         classNameBtn="btn__primary btn__primary--bigger"
         text="Entrar"
       />
+      <Input 
+      label = "Teste"
+      type="text"
+      text="Entrar"
+      placeholder="Nome"/>
     </div>
   );
 }
