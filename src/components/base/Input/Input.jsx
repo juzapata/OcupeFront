388fb8a77207112bb55ad0bcf1752ddd8127d0a8
Tatiane Mendes Camillo <tatiane@Alexs-MacBook-Pro.local>
2021-01-16 16:36:52 -0300

    icon add ao input

--- src/components/base/Input/Input.jsx
+++ src/components/base/Input/Input.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 
-
 const Input = (props) => {
     return (
         <div className="input-div">
-            <label 
-                className="input-label" 
+            <label
+                className="input-label"
                 htmlFor={props.id}>
-                    {props.label} 
-            </label>
-            <input 
+                {props.label}
+            <input
                 className="input-itself"
-                type={props.type} 
-                id={props.id} 
+                type={props.type}
+                id={props.id}
                 value={props.value}
                 aria-required="true"
-                placeholder={props.placeholder}
-            />
+                placeholder={props.placeholder} />
+                <i className={props.icon}></i>
+            </label>
         </div>
+        
     )
 }
 
