100638975839099bc40332ccae82040da2b7cbbe
NayamaRosa <falecom@nayama.com.br>
2021-01-17 00:27:10 -0300

    updating input style and html

--- src/components/base/Input/Input.jsx
+++ src/components/base/Input/Input.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 
 const Input = (props) => {
     return (
-        <div className="input-div">
-            <label
-                className="input-label"
-                htmlFor={props.id}>
-                {props.label}
+        <label
+            className="input__label"
+            htmlFor={props.id}>
+            {props.label}
             <input
-                className="input-itself"
+                className="input__itself"
                 type={props.type}
                 id={props.id}
                 value={props.value}
                 aria-required="true"
-                placeholder={props.placeholder} />
-                <i className={props.icon}></i>
-            </label>
-        </div>    
+                placeholder={props.placeholder} 
+                onChange={props.onChange}
+            />
+            <i className={`input__icon ${props.icon}`}></i>
+        </label> 
     )
 }
 
