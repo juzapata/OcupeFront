67c69a9ad09fd71ba1849802372609512f7ce7eb
NayamaRosa <falecom@nayama.com.br>
2021-01-17 17:49:49 -0300

    validate login

--- src/containers/SignIn/SignIn.jsx
+++ src/containers/SignIn/SignIn.jsx
@@ -6,22 +6,59 @@ import Image from '../../components/base/Image';
 import Cta from '../../components/base/Cta';
 import { Link } from 'react-router-dom';
 
-const initialValues = {
-  email: "",
-  senha: "",
-}
+// const initialValues = {
+//   email: "",
+//   senha: "",
+// }
 
 const SignIn = () => {
-  const [values, setValues] = useState(initialValues);
+  // const [values, setValues] = useState(initialValues);
 
-  const handleLogin = (e) => { 
-    const { name, value } = e.target;
+  // const handleLogin = (e) => { 
+  //   const { name, value } = e.target;
     
-    setValues({
-      ...values,
-      [name]: value,
-    });
-    console.log(values)
+  //   setValues({
+  //     ...values,
+  //     [name]: value,
+  //   });
+  //   console.log(values)
+  // }
+  const [inputEmailValue, setInputEmailValue] = useState('');
+  const [inputPasswordValue, setInputPasswordValue] = useState('');
+  const [mentorados, setMentorados] = useState([]);
+  let history = useHistory();
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        await fetch('https://ocupe-back-end.herokuapp.com/ocupe/get-mentored')
+          .then((res) => res.json())
+          .then(data => setMentorados(data.mentorados));
+      } catch (e) {
+          console.error(e);
+      }
+  };
+  fetchData();
+}, []);
+
+  const handleEmailValue = (e) => {
+    let inputValue = e.target.value; 
+    setInputEmailValue(inputValue);
+  }
+
+  const handlePasswordValue = (e) => {
+    let inputValue = e.target.value; 
+    setInputPasswordValue(inputValue);
+  }
+
+  const clickLogin = (e) => {
+    e.preventDefault();
+      if((inputEmailValue === mentorados[0].contact.email && inputPasswordValue == mentorados[0].password) ||
+          (inputEmailValue === mentorados[1].contact.email && inputPasswordValue == mentorados[1].password)) {
+        history.push('/home');
+      } else {
+        alert('Dados incorretos!');
+      }
   }
   
   return (    
@@ -36,22 +73,20 @@ const SignIn = () => {
           label="E-mail"
           placeholder="seuemail@exemplo.com"
           type="email"
-          value={values.email}
           name="email"
-          onChange= {handleLogin}/>
+          onChange= {(e) => handleEmailValue(e)}/>
         <Input
           label="Senha"
           placeholder="mínimo 6 caracteres"
           type="password"
-          value={values.senha}
           name="senha"
-          onChange= {handleLogin}/>
-        <Link to="/home">
+          onChange= {(e) => handlePasswordValue(e)}/>
           <Button 
           type="submit"
           classNameBtn="btn__primary"
-          text="Entrar"/>
-        </Link>
+          text="Entrar"
+          onClick={(e) => clickLogin(e)}
+        />
         <Cta
         classNameCta="cta__primary"
         text="Cadastre-se"
