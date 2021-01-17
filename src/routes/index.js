eab2929b853d509481154b154823caa4c3307bb3
NayamaRosa <falecom@nayama.com.br>
2021-01-17 00:29:40 -0300

    adding Search to route.js and adding ListItem.scss to main

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Home from './Home';
+import Login from './Login';
+import Search from './Search';
+
+const Routes = () => (
+//   <Switch>
+    // <Route exact path="/">
+      // <Home />
+      // <Login />
+      <Search />
+    // </Route>
+//   </Switch>
+);
+
+export default Routes;
